c6c0acc2a2ac5e7e0993ef30e4ca41acbe87408c
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-05-15 23:19:25 +0330

    add base.html and home.html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,7 @@
 from django.views import View
 from django.shortcuts import render
-from django.http.response import HttpResponse
 
 
 class HomeView(View):
     def get(self, request):
-        return HttpResponse('welcome to home')
+        return render(request, 'home/home.html')
