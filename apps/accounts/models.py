e21bd2f02c06b6550aa3da3b795b16f05012c620
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-05-20 21:33:51 +0330

    add to CustomUser - email and photo

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    photo = models.ImageField(upload_to='user_images/', null=True, blank=True)
